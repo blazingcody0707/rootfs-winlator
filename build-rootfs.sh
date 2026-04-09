228f2f497ebb8b5d96c9e3b435f0f7dc8a987707
Waim908 <waimxj449@foxmail.com>
2026-04-10 00:38:51 +0800

    init commits

---
+++ build-rootfs.sh
@@ -0,0 +1,432 @@
+extra_pkg=(
+  7zip
+)
+#pacman -S --needed --noconfirm ${extra_pkg[@]}
+
+export TZ=Asia/Shanghai
+export RootDirectories=(
+  home
+  opt
+  storage
+  usr/etc
+  usr/bin
+  usr/lib
+  usr/share
+  usr/libexec
+  usr/include
+  usr/games
+  usr/src
+  usr/sbin
+  usr/tmp
+  usr/var/run
+)
+export meson_general_arg=(
+  --buildtype=release
+  --strip
+  --prefix=/data/data/com.winlator/files/imagefs/usr
+  --libdir=/data/data/com.winlator/files/imagefs/usr/lib
+  --bindir=/data/data/com.winlator/files/imagefs/usr/bin
+  --sysconfdir=/data/data/com.winlator/files/imagefs/etc
+  --libexecdir=/data/data/com.winlator/files/imagefs/usr/libexec
+  --localstatedir=/data/data/com.winlator/files/imagefs/var
+  --datadir=/data/data/com.winlator/files/imagefs/usr/share
+  --includedir=/data/data/com.winlator/files/imagefs/usr/include
+  --sbindir=/data/data/com.winlator/files/imagefs/usr/sbin
+)
+apply_patch() {
+  if [[ ! -d /tmp/patches ]]; then
+    echo "pataches dir is not fonund!"
+    exit 1
+  fi
+  if [[ -d /tmp/patches/$1/$2 ]]; then
+    for i in `ls /tmp/patches/$1/$2`; do
+      if ! patch -p1 < /tmp/patches/$1/$2/$i; then
+        echo "Apply $i for $1/$2 failed"
+        exit 1
+      fi
+    done
+  else
+    echo "No Version Patch files=>$1/$2"
+  fi
+}
+
+patchelf_fix() {
+  LD_RPATH=/data/data/com.winlator/files/imagefs/usr/lib
+  LD_FILE=$LD_RPATH/ld-linux-aarch64.so.1
+  find . -type f -exec file {} + | grep -E ":.*ELF" | cut -d: -f1 | while read -r elf_file; do
+    echo "Patching $elf_file..."
+    patchelf --set-rpath "$LD_RPATH" --set-interpreter "$LD_FILE" "$elf_file" || {
+      echo "Failed to patch $elf_file" >&2
+      continue
+    }
+  done
+}
+
+strip_all() { find . -type f -exec file {} \; | grep ELF | cut -d: -f1 | xargs -r strip; }
+
+wget_source() {
+  command -v wget || exit 1
+  local target_package=$(basename $1)
+  wget $1 || { echo "下载失败" && exit 1;}
+  local target_dir=$(tar tf $target_package | head -n1)
+  tar xvf $target_package || { echo "解压失败" && exit 1;}
+  cd $target_dir
+}
+
+create_ver_txt() {
+  cat >'/data/data/com.winlator/files/imagefs/_version_.txt' <<EOF
+Output Date(UTC+8): $date
+Version:
+  xz=> $xzVer
+  flac=> $flacVer
+  glib=> $glibVer
+  gstreamer=> $gstVer
+  xkbcommon=> $xkbcommonVer
+  mangohud=> $mangohudVer
+  imagefs-tag=> $customTag
+  Extra_URL:
+    ${extraURL[@]}
+
+GitHub:
+  [Waim908/rootfs-winlator](https://github.com/Waim908/rootfs-winlator)
+EOF
+}
+
+clean_old_depends () {
+  rm -rf /data/data/com.winlator/files/imagefs/usr/lib/libFLAC++.so.10*
+  rm -rf /data/data/com.winlator/files/imagefs/usr/lib/libFLAC.so.12*
+  rm -rf /data/data/com.winlator/files/imagefs/usr/lib/lib/libglib-2.0.so.0.8000.3
+}
+
+################
+# Compile Args #
+################
+
+# 使用O2是为了稳定性和性能的平衡
+
+export CFLAGS="-O2 -march=armv8-a -mtune=generic -flto=auto"
+export CXXFLAGS="-O2 -march=armv8-a -mtune=generic -flto=auto"
+export LDFLAGS="-O2 -flto=auto -s"
+
+if [[ ! -f /tmp/init.sh ]]; then
+  exit 1
+else
+  cat /tmp/init.sh
+  source /tmp/init.sh
+fi
+# pacman -R --noconfirm flac
+create_imagefs_dir() {
+  mkdir -p /data/data/com.winlator/files/imagefs/
+  nowPath=$(pwd)
+  imagefsDir=/data/data/com.winlator/files/imagefs/
+  for i in ${!RootDirectories[@]}; do
+    mkdir -p $imagefsDir/${RootDirectories[i]} || exit 1
+  done
+  cd $imagefsDir
+  ln -sf usr/bin
+  ln -sf usr/lib
+  ln -sf usr/etc
+  ln -sf usr/tmp
+  ln -sf usr/var
+
+  cd $nowPath
+}
+create_imagefs_dir
+cd /tmp
+if ! wget https://github.com/Waim908/rootfs-winlator/releases/download/rootfs-ori-7.1.5/imagefs.txz; then
+  exit 1
+fi
+#tar -xf imagefs.txz -C /data/data/com.winlator/files/imagefs/
+#tar -xf data.tar.xz -C /data/data/com.winlator/files/imagefs/
+#tar -xf tzdata-*-.pkg.tar.xz -C /data/data/com.winlator/files/imagefs/
+cd /data/data/com.winlator/files/imagefs/etc
+mkdir ca-certificates
+cd ca-certificates
+if ! wget https://curl.haxx.se/ca/cacert.pem; then
+  exit 1
+fi
+cd /tmp
+#git clone https://github.com/xiph/flac.git flac-src
+if ! git clone -b $xzVer https://github.com/tukaani-project/xz.git xz-src; then
+  exit 1
+fi
+#git clone https://github.com/xiph/opus.git opus-src
+if ! git clone -b $gstVer https://github.com/GStreamer/gstreamer.git gst-src; then
+  exit 1
+fi
+
+git clone -b $xkbcommonVer https://github.com/xkbcommon/libxkbcommon.git xkbcommon-src || exit 1
+
+if [[ ! $mangohudVer == cmod ]]; then
+  git clone -b $mangohudVer https://github.com/flightlessmango/MangoHud.git mangohud-src || exit 1
+else
+  echo "Use Winlator Glibc mangohud"
+fi
+
+git clone -b $flacVer https://github.com/xiph/flac.git flac-src || exit 1
+
+git clone -b $glibVer https://github.com/GNOME/glib.git glib-src || exit 1
+
+pip install mako --break-system-package
+
+cd /tmp/xkbcommon-src
+
+meson setup builddir ${meson_general_arg[@]} \
+  -Dbash-completion-path=false \
+  -Denable-xkbregistry=false \
+  -Denable-wayland=false \
+  -Denable-tools=false \
+  -Denable-bash-completion=false || exit 1
+meson compile -C builddir || exit 1
+meson install -C builddir
+
+if [[ ! $mangohudVer == cmod ]]; then
+  cd /tmp/mangohud-src
+  apply_patch mangohud $mangohudVer
+
+  meson setup builddir ${meson_general_arg[@]} \
+    -Ddynamic_string_tokens=false \
+    -Dwith_xnvctrl=disabled \
+    -Dwith_wayland=disabled \
+    -Dwith_nvml=disabled \
+    -Dinclude_doc=false || exit 1
+  meson compile -C builddir || exit 1
+  meson install -C builddir
+else
+  if [[ -f /tmp/mangohud.tar.xz ]]; then
+    tar xf /tmp/mangohud.tar.xz -C /data/data/com.winlator/files/imagefs/ || exit 1
+  else
+    echo "/tmp/mangohud.tar.xz No such file"
+    exit 1
+  fi
+fi
+# Build
+echo "Build and Compile xz(liblzma)"
+cd /tmp/xz-src
+./autogen.sh
+mkdir build
+cd build
+if ! ../configure --prefix=/data/data/com.winlator/files/imagefs/usr; then
+  exit 1
+fi
+if ! make -j$(nproc); then
+  exit 1
+fi
+make install
+
+# mp3lame https://sourceforge.net/projects/lame/files/latest/download
+
+#cd /tmp/lame-3.100
+#./configure --prefix=/data/data/com.winlator/files/imagefs/usr/ || exit 1
+#make -j$(nproc) || exit 1
+#make install
+#
+# FLAC
+
+cd /tmp/flac-src
+if ! ./autogen.sh; then
+  exit 1
+fi
+if ! ./configure --prefix=/data/data/com.winlator/files/imagefs/usr/; then
+  exit 1
+fi
+if ! make -j$(nproc); then
+  exit 1
+fi
+make install
+
+# (removed vorbis section)
+
+# glib
+cd /tmp/glib-src/
+git submodule update --init --recursive || exit 1
+
+apply_patch glib $glibVer
+
+echo "Build and Compile glib"
+meson setup builddir ${meson_general_arg[@]} -Dglib_debug=disabled -Ddocumentation=false -Dintrospection=disabled -Dman-pages=disabled -Dselinux=disabled -Dtests=false   -Dlibmount=disabled \
+  -Druntime_dir=/data/data/com.winlator/files/imagefs/var/run || exit 1
+meson compile -C builddir || exit 1
+meson install -C builddir
+
+# xkeyboard-config
+wget_source https://xorg.freedesktop.org/archive/individual/data/xkeyboard-config/xkeyboard-config-2.47.tar.xz
+meson setup builddir ${meson_general_arg[@]} \
+  -Dcompat-rules=true \
+  -Dxorg-rules-symlinks=true \
+  -Dnls=false || exit 1
+meson compile -C builddir || exit 1
+meson install -C builddir
+
+cd ..
+
+# libxkb-file
+wget_source https://xorg.freedesktop.org/releases/individual/lib/libxkbfile-1.2.0.tar.xz
+meson setup builddir ${meson_general_arg[@]}  || exit 1
+meson compile -C builddir || exit 1
+meson install -C builddir
+
+cd ..
+
+cd /tmp/gst-src
+echo "Build and Compile gstreamer"
+meson setup builddir ${meson_general_arg[@]} \
+  -Dgst-full-target-type=shared_library \
+  -Dintrospection=disabled \
+  -Dgst-full-libraries=app,video,player \
+  -Dbase=enabled \
+  -Dgood=enabled \
+  -Dbad=enabled \
+  -Dugly=enabled \
+  -Dlibav=enabled \
+  -Dtests=disabled \
+  -Dexamples=disabled \
+  -Ddoc=disabled \
+  -Dges=disabled \
+  -Dpython=disabled \
+  -Ddevtools=disabled \
+  -Drtsp_server=disabled \
+  -Dlibnice=disabled \
+  -Dgtk=disabled \
+  -Dgstreamer:check=disabled \
+  -Dgstreamer:benchmarks=disabled \
+  -Dgstreamer:libunwind=disabled \
+  -Dgstreamer:libdw=disabled \
+  -Dgstreamer:bash-completion=disabled \
+  -Dgst-plugins-base:tcp=disabled \
+  -Dgst-plugins-base:typefind=disabled \
+  -Dgst-plugins-base:gio=disabled \
+  -Dgst-plugins-good:cairo=disabled \
+  -Dgst-plugins-good:gdk-pixbuf=disabled \
+  -Dgst-plugins-good:oss=disabled \
+  -Dgst-plugins-good:oss4=disabled \
+  -Dgst-plugins-good:v4l2=disabled \
+  -Dgst-plugins-good:aalib=disabled \
+  -Dgst-plugins-good:jack=disabled \
+  -Dgst-plugins-good:pulse=enabled \
+  -Dgst-plugins-good:adaptivedemux2=disabled \
+  -Dgst-plugins-good:libcaca=disabled \
+  -Dgst-plugins-good:mpg123=enabled \
+  -Dgst-plugins-good:multifile=disabled \
+  -Dgst-plugins-good:rtp=disabled \
+  -Dgst-plugins-good:rtpmanager=disabled \
+  -Dgst-plugins-good:rtsp=disabled \
+  -Dgst-plugins-good:soup=disabled \
+  -Dgst-plugins-good:udp=disabled \
+  -Dgst-plugins-good:aalib=disabled \
+  -Dgst-plugins-base:examples=disabled \
+  -Dgst-plugins-base:alsa=enabled \
+  -Dgst-plugins-base:pango=disabled \
+  -Dgst-plugins-base:x11=enabled \
+  -Dgst-plugins-base:gl=disabled \
+  -Dgst-plugins-base:opus=disabled \
+  -Dgst-plugins-bad:androidmedia=disabled \
+  -Dgst-plugins-bad:rtmp=disabled \
+  -Dgst-plugins-bad:shm=disabled \
+  -Dgst-plugins-bad:zbar=disabled \
+  -Dgst-plugins-bad:webp=disabled \
+  -Dgst-plugins-bad:kms=disabled \
+  -Dgst-plugins-bad:vulkan=disabled \
+  -Dgst-plugins-bad:dash=disabled \
+  -Dgst-plugins-bad:analyticsoverlay=disabled \
+  -Dgst-plugins-bad:nvcodec=disabled \
+  -Dgst-plugins-bad:uvch264=disabled \
+  -Dgst-plugins-bad:v4l2codecs=disabled \
+  -Dgst-plugins-bad:udev=disabled \
+  -Dgst-plugins-bad:libde265=disabled \
+  -Dgst-plugins-bad:smoothstreaming=disabled \
+  -Dgst-plugins-bad:fluidsynth=disabled \
+  -Dgst-plugins-bad:inter=disabled \
+  -Dgst-plugins-bad:x11=enabled \
+  -Dgst-plugins-bad:gl=disabled \
+  -Dgst-plugins-bad:wayland=disabled \
+  -Dgst-plugins-bad:openh264=disabled \
+  -Dgst-plugins-bad:hip=disabled \
+  -Dgst-plugins-bad:aja=disabled \
+  -Dgst-plugins-bad:aes=disabled \
+  -Dgst-plugins-bad:dtls=disabled \
+  -Dgst-plugins-bad:hls=disabled \
+  -Dgst-plugins-bad:curl=disabled \
+  -Dgst-plugins-bad:opus=disabled \
+  -Dgst-plugins-bad:webrtc=disabled \
+  -Dgst-plugins-bad:webrtcdsp=disabled \
+  -Dgst-plugins-bad:debugutils=disabled \
+  -Dgst-plugins-bad:librfb=disabled \
+  -Dgst-plugins-bad:rist=disabled \
+  -Dgst-plugins-bad:rtmp2=disabled \
+  -Dgst-plugins-bad:rtp=disabled \
+  -Dgst-plugins-bad:srtp=disabled \
+  -Dgst-plugins-bad:sdp=disabled \
+  -Dgst-plugins-bad:tensordecoders=disabled \
+  -Dgst-plugins-bad:unixfd=disabled \
+  -Dgst-plugins-bad:cuda-nvmm=disabled \
+  -Dgst-plugins-bad:fluidsynth=disabled \
+  -Dgst-plugins-ugly:asfdemux=disabled \
+  -Dpackage-origin="builtin-https://github.com/Waim908/rootfs-winlator" || exit 1
+if [[ ! -d builddir ]]; then
+  exit 1
+fi
+if ! meson compile -C builddir; then
+  exit 1
+fi
+meson install -C builddir
+export date=$(TZ=Asia/Shanghai date '+%Y-%m-%d %H:%M:%S')
+# package
+echo "Package"
+mkdir /tmp/output
+cd /data/data/com.winlator/files/imagefs/
+#rm -rf lib/*.a
+patchelf_fix
+##############
+create_ver_txt
+##############
+
+if ! tar -I 'xz -T$(nproc) -9' -cf /tmp/output/output-lite-${customTag}.tar.xz .; then
+  exit 1
+fi
+
+cd /tmp
+
+tar -xf data.tar.xz -C /data/data/com.winlator/files/imagefs/
+
+tar -xf tzdata-*-1-aarch64.pkg.tar.xz -C /data/data/com.winlator/files/imagefs/
+rm -rf /data/data/com.winlator/files/imagefs/.BUILDINFO
+rm -rf /data/data/com.winlator/files/imagefs/.MTREE
+rm -rf /data/data/com.winlator/files/imagefs/.PKGINFO
+
+
+cd /data/data/com.winlator/files/imagefs/
+if ! tar -I 'xz -T$(nproc) -9' -cf /tmp/output/output-full-${customTag}.tar.xz .; then
+  exit 1
+fi
+cd /tmp
+rm -rf /data/data/com.winlator/files/imagefs/
+create_imagefs_dir
+tar -xf imagefs.txz -C /data/data/com.winlator/files/imagefs/
+cd /data/data/com.winlator/files/imagefs/
+##############
+#clean_old_depends
+##############
+cd usr/lib
+
+ln -sf libvorbis.so.0.4.9 libvorbis.so.0
+ln -sf libvorbisenc.so.2.0.12 libvorbisenc.so.2
+ln -sf libvorbisfile.so.3.3.8 libvorbisfile.so.3
+##############
+echo "Listing usr/lib:"
+echo "==============================="
+ls -l
+echo "==============================="
+##############
+cd /data/data/com.winlator/files/imagefs/
+##############
+rm -rf /data/data/com.winlator/files/imagefs/usr/lib/libgst*
+rm -rf /data/data/com.winlator/files/imagefs/usr/lib/gstreamer-1.0/
+#######
+#strip_all
+#######
+tar -xf /tmp/output/output-full-${customTag}.tar.xz -C /data/data/com.winlator/files/imagefs/
+#create_ver_txt
+if ! tar -I 'zstd -T$(nproc) -9' -cf /tmp/output/imagefs-${customTag}.tzst .; then
+  exit 1
+fi
